5300433637c58554972a2a0117c51b63e639cd30
Razi <razi.94@outlook.com>
2018-10-03 17:53:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+// Instead of using a HOC, we can share code using a
+// regular component with a render prop!
+class Mouse extends React.Component {
+  static propTypes = {
+    render: PropTypes.func.isRequired
+  };
+
+  state = { x: 0, y: 0 };
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <div style={{ minHeight: "100vh", minWidth: "100vw" }}>
+    <Mouse
+      render={({ x, y }) => (
+        // The render prop gives us the state we need
+        // to render whatever we want here.
+        <h1>
+          The mouse position is ({x}, {y})
+        </h1>
+      )}
+    />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
